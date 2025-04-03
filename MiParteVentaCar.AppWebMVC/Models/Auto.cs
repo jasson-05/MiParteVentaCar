11977ca30db0d04ee1de1bc8d60aef1ef60294ab
Jasso <Jasso@DESKTOP-97AP7FC>
2025-04-02 20:09:12 -0600

    diseño a la vista crear de autos

--- MiParteVentaCar.AppWebMVC/Models/Auto.cs
+++ MiParteVentaCar.AppWebMVC/Models/Auto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MiParteVentaCar.AppWebMVC.Models;
 
@@ -13,20 +14,32 @@ public partial class Auto
 
     public int IdMarca { get; set; }
 
+
     public string? AnnoFabricacion { get; set; }
 
+    [Required(ErrorMessage = "El modelo es obligatorio.")]
+    [Display(Name = "Modelo")]
     public string Modelo { get; set; } = null!;
 
+    [Required(ErrorMessage = "La Descripción es obligatoria.")]
+    [Display(Name = "Descripción")]
     public string DescripcionA { get; set; } = null!;
 
     public decimal? Kilometraje { get; set; }
 
     public string? Estado { get; set; }
 
+    [Required(ErrorMessage = "El precio es obligatorio.")]
+    [Display(Name = "Precio")]
     public decimal Precio { get; set; }
 
+
+    [Required(ErrorMessage = "La Foto es obligatoria.")]
+    [Display(Name = "Foto")]
     public string Urlimagen { get; set; } = null!;
 
+    [Required(ErrorMessage = "La utima revición tecnológica es obligatoria.")]
+    [Display(Name = "Utima revición tecnológica")]
     public DateTime Urt { get; set; }
 
     public DateTime? FechaRp { get; set; }
